ff4f784290c1154545993a454026868e7dc5e2c0
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-08-24 23:56:36 +0300

    update path to WelcomePage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,7 @@
-import { lazy } from 'react'
-import { Route, Routes   } from "react-router-dom";
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-
-const WelcomePage = lazy(() => import('../pages/WelcomePage'));
+const WelcomePage = lazy(() => import('../pages/WelcomePage/WelcomePage'));
 const SingupPage = lazy(() => import('../pages/SingupPage'));
 const SinginPage = lazy(() => import('../pages/SinginPage'));
 const MainPage = lazy(() => import('../pages/MainPage'));
@@ -11,30 +10,22 @@ const AddRecipePage = lazy(() => import('../pages/AddRecipePage'));
 const RecipePage = lazy(() => import('../pages/RecipePage'));
 const MyRecipesPage = lazy(() => import('../pages/MyRecipesPage'));
 
-
-
-
-
 export const App = () => {
   return (
-     <Routes>
+    <Routes>
       <Route path="/welcome" element={<WelcomePage />} />
-      
+
       <Route path="/singup" element={<SingupPage />} />
       <Route path="/singin" element={<SinginPage />} />
-
-
-   
 
       <Route path="/main" element={<MainPage />} />
       <Route path="/drinks/:categoryName	" element={<DrinksPage />} />
 
+      <Route path="/add" element={<AddRecipePage />} />
+      <Route path="/recipe/:recipeId" element={<RecipePage />} />
+      <Route path="/my" element={<MyRecipesPage />} />
 
-        <Route path="/add" element={<AddRecipePage />} />
-        <Route path="/recipe/:recipeId" element={<RecipePage />} />
-         <Route path="/my" element={<MyRecipesPage />} /> 
-        
-      <Route path="*" element={<WelcomePage />} /> 
+      <Route path="*" element={<WelcomePage />} />
     </Routes>
   );
 };
