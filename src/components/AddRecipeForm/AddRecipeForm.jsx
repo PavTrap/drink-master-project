8cb0c69d0ee042393cf5417adae3008b231351d1
Chris <kisunya750@gmail.com>
2023-08-24 21:45:42 +0300

    fix2

--- src/components/AddRecipeForm/AddRecipeForm.jsx
+++ src/components/AddRecipeForm/AddRecipeForm.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 // import s from './AddRecipeForm.module.css'
 
  
--- src/components/PopularRecipe/PopularRecipe.jsx
+++ src/components/PopularRecipe/PopularRecipe.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 // import s from './PopularRecipe.module.css'
  
 export const PopularRecipe = () => {
