466dbd2eb0701cbb54ddeaf55dcd704e518c19b9
Chris <kisunya750@gmail.com>
2023-08-25 14:12:16 +0300

    form

--- src/components/AddRecipeForm/AddRecipeForm.jsx
+++ src/components/AddRecipeForm/AddRecipeForm.jsx
@@ -1,12 +1,23 @@
 // import PropTypes from 'prop-types';
-// import s from './AddRecipeForm.module.css'
+import s from './AddRecipeForm.module.css'
+import { RecipeDescriptionFields } from '../RecipeDescriptionFields/RecipeDescriptionFields';
+ import { RecipeIngredientsFields } from '../RecipeIngredientsFields/RecipeIngredientsFields';
+import { RecipePreparationFields } from '../RecipePreparationFields/RecipePreparationFields';
 
  
 export const AddRecipeForm = () => {
       
       return (
            
-            <></>
+            <>
+                  <form >
+                  <RecipeDescriptionFields />
+                  <RecipeIngredientsFields />
+                  <RecipePreparationFields />
+                        <button type='submit' className={s.add_btn}>Add</button>
+                  
+                  </form>
+            </>
            
             )
 }
