143c4da2120efb0e2e322c2eed874673fc9bd037
Serhii Naboka <drifterzzz@gmail.com>
2023-08-26 22:43:27 +0300

    hkjh

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -6,11 +6,11 @@ import css from './MainPage.module.css';
 import axios from 'axios';
 // import { createAsyncThunk } from '@reduxjs/toolkit';
 import { MainTitle } from 'components/MainTitle/MainTitle';
-// import { useEffect } from 'react';
+// import { useSelector } from 'react-redux';
 
 const HERO_TITLE = `Craft Your Perfect Drink with Drink Master`;
 const BASE_URL = 'https://drink-master-back-end.onrender.com';
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZTg4ZmIyMjQ0Nzc2MjQ3ZmRjOTFmMiIsImlhdCI6MTY5MzA1NjA1NiwiZXhwIjoxNzI0NTkyMDU2fQ.5Q6N6FLJXdNQSnE2SnjhkzLvnwWX3iiohdZppQRRVhY'
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZTg4ZmIyMjQ0Nzc2MjQ3ZmRjOTFmMiIsImlhdCI6MTY5MzA3ODY5NSwiZXhwIjoxNzI0NjE0Njk1fQ.WmwrHdAin8J2FTpCqc3JrQk9-QREB7bcQbdGUDuRaLI'
 // const setToken = token =>
 //   (axios.defaults.headers.common.Authorization = `Bearer ${token}`);
 
@@ -21,42 +21,16 @@ const fetchDrinksInstance = axios.create({
 	},
 });
 
-const getAlldrinks = state => state;
-
-// export const setToken = token => {
-// 	fetchDrinksInstance.defaults.headers.common['Authorization'] = token;
-// };
-
-// headers: { ...config.headers, Authorization: `Bearer ${token}` }
-
-// export const fetchDrinks = createAsyncThunk(
-//   'drinks/fetchAll',
-//   async (_, { rejectWithValue }) => {
-//     try {
-// 			const { data} = await fetchDrinksInstance('/api/recipes');
-// 			console.log(data[0].data);
-// 			return data[0].data;
-//     } catch (e) {
-//       console.log(e);
-//       return rejectWithValue(e.response.data.message);
-//     }
-//   }
-// );
-
-// setToken(TOKEN);
-
 
 export const fetchDrinks = async () => {
 	try {
 		const { data } = await fetchDrinksInstance('/api/recipes/main-page');
-		// console.log(res);
 		console.log(data);
-		// console.log(data.data);
 		return data;
 	} catch (e) {
 		console.log(e);
 	}
-}
+};
 
 export const DrinkCard = ({ drink, drinkThumb }) =>
 (<li >
@@ -66,32 +40,28 @@ export const DrinkCard = ({ drink, drinkThumb }) =>
 </li>)
 
 
+
 const MainPage = () => {
 	const [allDrinks, setAllDrinks] = useState([]);
-	// const [ordinaryDrinks, setOrdinaryDrinks] = useState([]);
+	const [ordinaryDrinks, setOrdinaryDrinks] = useState([]);
 	// const dispatch = useDispatch();
-	// const isLoading = useSelector(getIsLoading);
-	// const error = useSelector(getError);
-	// const isLoginned = useSelector(getIsLoginned)
+
+	const findOrdinaryDrinks = drinks => {
+		const allOrdinaryDrinks = drinks.filter(drink => drink.category === "Ordinary Drink");
+		return allOrdinaryDrinks[0];
+	};
 
 	useEffect(() => {
 		fetchDrinks()
-			.then(res => setAllDrinks(res))
+			.then(res => {
+				setAllDrinks(res);
+				const ordinaryDrink = findOrdinaryDrinks(allDrinks);
+				setOrdinaryDrinks(ordinaryDrink)
+			})
 			.catch(err => console.log(err))
 	}, []);
 
-	// const findOrdinaryDrinks = drinks => {
-	// 	const allOrdinaryDrinks = drinks.filter(drink => drink.category === "Ordinary Drink");
-	// 	const items = allOrdinaryDrinks[0].items;
 
-	// 	console.log(allOrdinaryDrinks[0].items)
-
-	// 	return items;
-	// };
-
-	// const ordinaryDrinks = findOrdinaryDrinks(allDrinks);
-
-	// console.log(ordinaryDrinks);
 
 
 	return (
