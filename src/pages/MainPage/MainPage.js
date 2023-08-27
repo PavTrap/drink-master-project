d48d5acebb35056549a63295330ea897bd3e5f63
Serhii Naboka <drifterzzz@gmail.com>
2023-08-27 11:12:28 +0300

    logic of request for main page and render cards

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 // import { useDispatch } from 'react-redux';
 // import { Link } from 'react-router-dom';
 import css from './MainPage.module.css';
+import { drinks } from './drinks';
 
 import axios from 'axios';
 // import { createAsyncThunk } from '@reduxjs/toolkit';
@@ -42,26 +43,59 @@ export const DrinkCard = ({ drink, drinkThumb }) =>
 
 
 const MainPage = () => {
-	const [allDrinks, setAllDrinks] = useState([]);
-	const [ordinaryDrinks, setOrdinaryDrinks] = useState([]);
+	const [allDrinks, setAllDrinks] = useState(null);
+	// const [ordinaryDrinks, setOrdinaryDrinks] = useState(null);
+	// const [cocktails, setCocktails] = useState(null);
+	// const [shakes, setshakes] = useState(null);
+	// const [otherDrinks, setOtherDrinks] = useState(null);
 	// const dispatch = useDispatch();
 
-	const findOrdinaryDrinks = drinks => {
-		const allOrdinaryDrinks = drinks.filter(drink => drink.category === "Ordinary Drink");
-		return allOrdinaryDrinks[0];
-	};
+	// const findOrdinaryDrinks = drinks => {
+	// 	const allOrdinaryDrinks = drinks.filter(drink => drink.category === "Ordinary Drink");
+	// 	return allOrdinaryDrinks[0];
+	// };
+
+	// const ordinaryDrink = findOrdinaryDrinks(allDrinks);
+	// console.log(ordinaryDrink)
 
 	useEffect(() => {
 		fetchDrinks()
 			.then(res => {
 				setAllDrinks(res);
-				const ordinaryDrink = findOrdinaryDrinks(allDrinks);
-				setOrdinaryDrinks(ordinaryDrink)
 			})
 			.catch(err => console.log(err))
 	}, []);
 
 
+	const separateDrinks = drinks => {
+		if (!allDrinks) return;
+
+		const ordinaryDrinks = [];
+		const cocktails = [];
+		const shakes = [];
+		const otherDrinks = [];
+
+		for (const drink of drinks) {
+			if (drink.category === "Ordinary Drink") ordinaryDrinks.push(drink);
+			if (drink.category === "Cocktail") cocktails.push(drink);
+			if (drink.category === "Snake") shakes.push(drink);
+			if (drink.category === "Other/Unknow") otherDrinks.push(drink);
+		};
+
+		const getedDrinks = {
+			"ordinary drink": ordinaryDrinks[0].items,
+			"coctail": cocktails[0].items,
+			"shake": shakes[0].items,
+			"other/unknown": otherDrinks[0].items,
+		};
+
+		console.log(getedDrinks);
+		return getedDrinks;
+	};
+
+	// separateDrinks(allDrinks);
+	const getedDrinks = separateDrinks(allDrinks);
+	// console.log(getedDrinks['ordinary drink']);
 
 
 	return (
@@ -77,14 +111,29 @@ const MainPage = () => {
 
 				{/* <Link to="/add">Add recipe</Link> */}
 				<h2>Ordinary Drink</h2>
-				<ul className={css.mainPageList}>
-					{/* {allDrinks.map(({ drink, drinkThumb, _id }) => (
+				{allDrinks && <ul className={css.mainPageList}>
+					{getedDrinks['ordinary drink'].map(({ drink, drinkThumb, _id }) => (
 						<DrinkCard key={_id} drink={drink} drinkThumb={drinkThumb}></DrinkCard>
-					))} */}
-				</ul>
+					))}
+				</ul>}
 				<h2>Cocktail</h2>
+				{allDrinks && <ul className={css.mainPageList}>
+					{getedDrinks.coctail.map(({ drink, drinkThumb, _id }) => (
+						<DrinkCard key={_id} drink={drink} drinkThumb={drinkThumb}></DrinkCard>
+					))}
+				</ul>}
 				<h2>Shake</h2>
+				{allDrinks && <ul className={css.mainPageList}>
+					{getedDrinks.shake.map(({ drink, drinkThumb, _id }) => (
+						<DrinkCard key={_id} drink={drink} drinkThumb={drinkThumb}></DrinkCard>
+					))}
+				</ul>}
 				<h2>Other/Unknow</h2>
+				{allDrinks && <ul className={css.mainPageList}>
+					{getedDrinks['other/unknown'].map(({ drink, drinkThumb, _id }) => (
+						<DrinkCard key={_id} drink={drink} drinkThumb={drinkThumb}></DrinkCard>
+					))}
+				</ul>}
 				{/* {error && <h3>{error}</h3>} */}
 
 				<a href="drinks">Other drinks</a>
