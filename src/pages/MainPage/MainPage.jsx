db3e50bd91f17f3f3f282f57e3a41cfdd3db2cff
Serhii Naboka <drifterzzz@gmail.com>
2023-08-27 14:26:41 +0300

    upd css

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -27,8 +27,10 @@ export const fetchDrinks = async () => {
 export const DrinkCard = ({ drink, drinkThumb }) =>
 (<li >
 	<img src={drinkThumb} alt='drink foto' height={400} />
-	<p>{drink}</p>
-	<a href='ingredients'><p>ingredients</p></a>
+	<div className={css.card_text_wrapper}>
+		<p className={css.card_name}>{drink}</p>
+		<a className={css.card_link} href='ingredients'><p className={css.ingredients_text}>ingredients</p></a>
+	</div>
 </li>)
 
 
