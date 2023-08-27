5a2d0e331a93a9edc80efae37f5377db57cb223c
Pavel_SoundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-08-28 02:16:49 +0300

    loginpage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ const RecipePage = lazy(() => import('../pages/RecipePage'));
 const MyRecipesPage = lazy(() => import('../pages/MyRecipesPage/MyRecipesPage'));
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
 const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
-const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
+const LoginPage = lazy(() => import('../pages/LogInPage/LoginPage'));
 export const App = () => {
   return (
     <Routes>
