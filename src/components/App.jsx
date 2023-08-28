7a991ebaaa843add45b4293d762ff2b58f907830
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-08-28 18:55:03 +0300

    +- похоже на правду

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,36 +7,18 @@ import OnlyGuest from './Routes/OnlyGuest';
 
 
 const WelcomePage = lazy(() => import('../pages/WelcomePage/WelcomePage'));
-const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
-// const MainPage = lazy(() => import('../pages/MainPage'));
+
+const MainPage = lazy(() => import('../pages/MainPage'));
 const DrinksPage = lazy(() => import('../pages/DrinksPage'));
 const AddRecipePage = lazy(() => import('../pages/AddRecipePage/AddRecipePage'));
 const RecipePage = lazy(() => import('../pages/RecipePage'));
 const MyRecipesPage = lazy(() => import('../pages/MyRecipesPage/MyRecipesPage'));
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
 const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
-
 const LoginPage = lazy(() => import('../pages/LogInPage/LoginPage'));
-const FavoritePage = lazy(()=>import('../pages/FavoritePage'))
-
 export const App = () => {
   return (
     <Routes>
-{/* 
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<WelcomePage />} />
-
-        <Route path='spinner' element={<Spinner />} />
-        <Route path="signin" element={<RestrictedRoute retirectTo={"/main"} component={<LoginPage />} />} />
-        <Route path="signup" element={<RestrictedRoute retirectTo={"/main"} component={<RegisterPage />} />} />
-        <Route path="main" element={<PrivateRoute retirectTo={"/"} component={<MainPage />} />} />
-        <Route path="drinks/:categoryName" element={<DrinksPage />} />
-        <Route path="add" element={<AddRecipePage />} />
-        <Route path="recipe/:recipeId" element={<RecipePage />} />
-        <Route path="my" element={<MyRecipesPage />} />
-        <Route path="/favorite" element={<FavoritePage/>} />
-        <Route path="*" element={<NotFoundPage />} />
-*/}
       <Route path="/" element={<Private component={<SharedLayout />} />}>
         <Route path="main" element={<Private component={<MainPage />} />} />
         <Route path="drinks" element={<Private component={<DrinksPage />} />} />
@@ -50,7 +32,6 @@ export const App = () => {
         <Route path="/welcome" element={<OnlyGuest component={<WelcomePage />} />} />
         <Route path="signin" element={<OnlyGuest component={<LoginPage />} />} />
         <Route path="signup" element={<OnlyGuest component={<RegisterPage />} />} />
-        <Route path="/favorite" element={<FavoritePage/>} />
       </Route>
       <Route path="*" element={<NotFoundPage />} />
       <Route path="spinner" element={<Spinner />} />
