082294ee60b5479b203138386f53bd19c83260fb
Chris <kisunya750@gmail.com>
2023-08-29 23:51:12 +0300

    add-recipe

---
+++ src/data/ingredients.js
@@ -0,0 +1,12 @@
+export const ingredients = [
+  { value: 'ocean', label: 'Ocean' },
+  { value: 'blue', label: 'Blue' },
+  { value: 'purple', label: 'Purple'  },
+  { value: 'red', label: 'Red'},
+  { value: 'orange', label: 'Orange' },
+  { value: 'yellow', label: 'Yellow' },
+  { value: 'green', label: 'Green'},
+  { value: 'forest', label: 'Forest' },
+  { value: 'slate', label: 'Slate' },
+  { value: 'silver', label: 'Silver' },
+];
