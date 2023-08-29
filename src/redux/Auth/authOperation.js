86f3316eac8700dec8795d333fadf23012265b13
NovicovMykhailo <mikenovicov@gmail.com>
2023-08-30 01:37:03 +0300

    Update authOperation.js

--- src/redux/Auth/authOperation.js
+++ src/redux/Auth/authOperation.js
@@ -1,56 +1,50 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+
 axios.defaults.baseURL = 'https://drink-master-back-end.onrender.com/';
 
-const setAuthHeader = token =>
-  (axios.defaults.headers.common.Authorization = `Bearer ${token}`);
-const cleanAuthHeader = () =>
-  (axios.defaults.headers.common.Authorization = '');
+const { token } = JSON.parse(localStorage.getItem('persist:auth'));
+const normatizedToken = JSON.parse(token);
+const config = {
+  headers: { Authorization: `Bearer ${normatizedToken}` },
+};
 
-
-   // Register 
-export const register = createAsyncThunk(
-  '/users/register',
-  async (user, { rejectWithValue }) => {
-    console.log(user)
-    try {
-      const response = await axios.post('/users/register', user);
-      setAuthHeader(response.data.token);
-       console.log(response)
-      return response.data;
-    } catch (e) {
-      return rejectWithValue(e.message);
-    }
+// Register
+export const register = createAsyncThunk('/users/register', async (user, { rejectWithValue }) => {
+  try {
+    const response = await axios.post('/users/register', user);
+    return response.data;
+  } catch (e) {
+    return rejectWithValue(e.message);
   }
-);
+});
 
-//  login 
-export const login = createAsyncThunk(
-  '/users/login',
-  async (user, { rejectWithValue }) => {
-    try {
-      const response = await axios.post('/users/login', user);
-      setAuthHeader(response.data.token);
-      return response.data;
-    } catch (e) {
-      return rejectWithValue(e.message);
-    }
+//  login
+export const login = createAsyncThunk('/users/login', async (user, { rejectWithValue }) => {
+  try {
+    const response = await axios.post('/users/login', user);
+    return response.data;
+  } catch (e) {
+    return rejectWithValue(e.message);
   }
-);
+});
 
-
-//  logOut 
-export const logOut = createAsyncThunk(
-  '/users/logout',
-  async (user, { rejectWithValue }) => {
-    try {
-      await axios.post('/users/logout', user);
-      cleanAuthHeader();
-    } catch (e) {
-      return rejectWithValue(e.message);
-    }
+//  refresh
+export const refreshUser = createAsyncThunk('/users/current', async (user, { rejectWithValue }) => {
+  try {
+    const response = await axios.get('/users/current', config);
+    return response.data;
+  } catch (e) {
+    return rejectWithValue(e.message);
   }
-);
+});
 
-
+//  logOut
+export const logOut = createAsyncThunk('/users/logout', async (user, { rejectWithValue }) => {
+  try {
+    await axios.post('/users/logout', config);
+  } catch (e) {
+    return rejectWithValue(e.message);
+  }
+});
