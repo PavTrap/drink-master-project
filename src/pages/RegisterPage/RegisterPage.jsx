fb99cfe0307722ea36faa1b26814c99fdc9ab287
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-08-31 00:37:10 +0300

    add BackgroundEffect for start

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,13 +1,17 @@
 import css from '../RegisterPage/RegisterPage.module.css';
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
+import BackgroundEffect from '../../components/BackgroundEffect/BackgroundEffect';
 
 export default function RegisterPage() {
   const { section, mainContainer } = css;
   return (
-    <div className={section}>
-      <div className={mainContainer}>
-        <RegisterForm />
+    <>
+      <BackgroundEffect />
+      <div className={section}>
+        <div className={mainContainer}>
+          <RegisterForm />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
