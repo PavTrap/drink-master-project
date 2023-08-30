38d4652e767f51702aa74575033b313ed35a92e3
Stanislav-Zhuk <steys.ua@gmail.com>
2023-08-30 23:56:37 +0300

    update

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -6,23 +6,38 @@ import RecipeIngredientsItem from 'components/RecipeIngredientsItem/RecipeIngred
 import RecipePreparation from 'components/RecipePreparation/RecipePreparation';
 import cocktailsData from '../data/cocktails.json';
 import { nanoid } from 'nanoid';
+import axios from 'axios';
+import setAuthHeader from 'helpers/axiosHedder';
+
+export const getRecipe = async ( token, recipeId ) => {
+  try {
+    setAuthHeader(token);
+    const { data } = await axios.get(`/api/recipes/${recipeId}`);
+    console.log(data);
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+// 639b6de9ff77d221f190c50f
 
 const RecipePage = () => {
   const { recipeId } = useParams();
   const selectedRecipe = cocktailsData.find(recipe => recipe._id.$oid === recipeId);
-  console.log(cocktailsData[4]._id.$oid)
-  console.log(cocktailsData[4])
+  // console.log(cocktailsData[0]._id.$oid)
+  // console.log(cocktailsData[0])
 
   return (
     <div>
       <section>
-        <RecipePageHero data={cocktailsData[4]} />
-        <RecipeIngredientsList data={cocktailsData[4]}>
-          {cocktailsData[4].ingredients.map(item => (
+        <RecipePageHero data={cocktailsData[0]} />
+        <RecipeIngredientsList data={cocktailsData[0]}>
+          {cocktailsData[0].ingredients.map(item => (
             <RecipeIngredientsItem data={item} key={nanoid()}/>
           ))}
         </RecipeIngredientsList>
-        <RecipePreparation data={cocktailsData[4]} />    
+        <RecipePreparation data={cocktailsData[0]} />    
       </section>
     </div>
   )
