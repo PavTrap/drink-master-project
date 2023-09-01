8117c8d880c435aa92b6242de475b35f8a280d57
NovicovMykhailo <mikenovicov@gmail.com>
2023-09-01 18:32:22 +0300

    finalize some styling

--- src/components/AddRecipeForm/RecipeIngredientsFields/RecipeIngredientsFields.jsx
+++ src/components/AddRecipeForm/RecipeIngredientsFields/RecipeIngredientsFields.jsx
@@ -1,13 +1,16 @@
-// // import PropTypes from 'prop-types';
-import css from './RecipeIngredientsFields.module.css';
+// import logic;
+import Select from 'react-select';
 import { useState, useEffect, useRef } from 'react';
+import { nanoid } from 'nanoid';
+//styles
+import css from './RecipeIngredientsFields.module.css';
+import {ingredientStyles, measureStyles} from "./inputStyles"
+//icons
 import { MdOutlineClose } from 'react-icons/md';
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
-
-import Select from 'react-select';
+// data
 import { fetchIngredients } from '../../../fetchAPI/fetchAPI';
 import measure from 'data/measure';
-import { nanoid } from 'nanoid';
 
 export const RecipeIngredientsFields = ({ addIngredients, addMeasure }) => {
   const [countIngredients, setCountIngredients] = useState(1);
@@ -28,12 +31,6 @@ export const RecipeIngredientsFields = ({ addIngredients, addMeasure }) => {
         setIngredients(result);
       })
       .catch(err => console.log(err));
-
-    //     fetchRecipes().then(res => {
-    //                   console.log(res.data.map(r => r.ingredients
-    // ))
-    //       })
-    //       .catch(err => console.log(err))
   }, []);
 
   useEffect(() => {
@@ -62,10 +59,11 @@ export const RecipeIngredientsFields = ({ addIngredients, addMeasure }) => {
       inputFields.push(
         <div className={css.addIngredients_box} key={nanoid()}>
           <Select
-            className="basic-single"
             ref={InselectRef}
-            classNamePrefix="select"
             isSearchable={true}
+            components={{
+              IndicatorSeparator: () => null,
+            }}
             name="ingredient"
             onChange={handlerSelect}
             options={allIngredients}
@@ -74,14 +72,15 @@ export const RecipeIngredientsFields = ({ addIngredients, addMeasure }) => {
           />
 
           <Select
-            className="basic-single"
-            classNamePrefix="select"
             ref={selectRef}
             isSearchable={true}
             name="measure"
             onChange={handlerSelect}
             options={measure}
             styles={measureStyles}
+            components={{
+              IndicatorSeparator: () => null,
+            }}
           />
 
           <button
@@ -125,40 +124,5 @@ export const RecipeIngredientsFields = ({ addIngredients, addMeasure }) => {
   );
 };
 
-const ingredientStyles = {
-  control: (baseStyles, state) => ({
-    ...baseStyles,
-    borderRadius: '60px',
-    width: '316px',
-    marginRight: '14px',
-    height: '56px',
-    paddingLeft: '14px',
-    backgroundColor: 'rgba(22, 31, 55, 0)',
-    color: 'white',
-  }),
-  option: base => ({
-    ...base,
 
-    color: 'white',
-    height: '100%',
-    backgroundColor: 'rgba(22, 31, 55, 1)',
-  }),
-};
 
-const measureStyles = {
-  control: (baseStyles, state) => ({
-    ...baseStyles,
-    borderRadius: '60px',
-    width: '150px',
-    marginRight: '40px',
-    height: '56px',
-    paddingLeft: '24px',
-    backgroundColor: 'rgba(22, 31, 55, 0)',
-  }),
-  option: base => ({
-    ...base,
-    color: 'white',
-    height: '100%',
-    backgroundColor: 'rgba(22, 31, 55, 1)',
-  }),
-};
