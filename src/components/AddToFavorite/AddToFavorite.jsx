c9eddc0dbdae21df02f98543cd2b5a6e12166bda
Stanislav-Zhuk <steys.ua@gmail.com>
2023-09-02 14:27:01 +0300

    update

--- src/components/AddToFavorite/AddToFavorite.jsx
+++ src/components/AddToFavorite/AddToFavorite.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import css from './AddToFavorite.module.css'
 
 const AddToFavorite = ({ id, onAdd }) => {
   const dispatch = useDispatch();
   return (
-    <button type="button" onClick={()=>dispatch(onAdd(id))}>Add to favorite recipe</button>
+    <button className={css.button} type="button" onClick={()=>dispatch(onAdd(id))}>Add to favorite recipe</button>
   )
 };
 
