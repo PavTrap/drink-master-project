6ce0def0f8f40bff448176226981ac0883dba8c3
HalynaSilina <halynasilina@gmail.com>
2023-09-02 10:35:59 +0300

    add images

--- src/pages/FavoritePage/FavoritePage.js
+++ src/pages/FavoritePage/FavoritePage.js
@@ -8,6 +8,7 @@ import { getFavoriteRecipes, getFavPage } from 'redux/FavoriteCocktails/Favorite
 import { deleteFavorites, fetchFavorites } from 'redux/FavoriteCocktails/FavoritesOperation';
 import { changeFavPage } from 'redux/FavoriteCocktails/FavoritesSlice';
 import css from './FavoritePage.module.css';
+import { NoRecipe } from 'components/NoRecipe/NoRecipe';
 
 export default function FavoritePage() {
   const favorites = useSelector(getFavoriteRecipes);
@@ -20,12 +21,12 @@ export default function FavoritePage() {
   return (
     <section className={css.favoritesContainer}>
       <MainTitle title="Favorites" />
-      {favorites.length !== 0 && (
+      {favorites.length !== 0 ?(
         <>
           <RecipesList recipes={favorites.data} state={{ from: location }} onDelete={deleteFavorites} />
           {favorites.count.totalPages > 1 && <Paginator pages={favorites.count} onChangePage={changeFavPage}/>}
         </>
-      )}
+      ): <NoRecipe title="You haven't added any favorite cocktails yet"/>}
     </section>
   );
 }
