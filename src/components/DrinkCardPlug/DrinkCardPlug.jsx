cf9e0d1fef5cfbe7d46794fb7847844fcffefffa
Serhii Naboka <drifterzzz@gmail.com>
2023-09-03 11:57:26 +0300

    upd css

--- src/components/DrinkCardPlug/DrinkCardPlug.jsx
+++ src/components/DrinkCardPlug/DrinkCardPlug.jsx
@@ -4,19 +4,19 @@ export const DrinkCardPlug = ({ drink, drinkThumb }) => {
 	return (
 		<>
 			<li className={css.mainPageList_item}>
-				<img src={drinkThumb} alt='drink' height={400} />
+				<img className={css.drinkPlugImg} src={drinkThumb} alt='drink' height={400} />
 				<div className={css.card_text_wrapper}>
 					<p className={css.card_name}>{drink}</p>
 				</div>
 			</li>
 			<li className={css.mainPageList_item}>
-				<img src={drinkThumb} alt='drink' height={400} />
+				<img className={css.drinkPlugImg} src={drinkThumb} alt='drink' height={400} />
 				<div className={css.card_text_wrapper}>
 					<p className={css.card_name}>{drink}</p>
 				</div>
 			</li>
 			<li className={css.mainPageList_item}>
-				<img src={drinkThumb} alt='drink' height={400} />
+				<img className={css.drinkPlugImg} src={drinkThumb} alt='drink' height={400} />
 				<div className={css.card_text_wrapper}>
 					<p className={css.card_name}>{drink}</p>
 				</div>
