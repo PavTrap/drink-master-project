a615f1e9d01def45f2e48405e02f72799e9a833f
HalynaSilina <halynasilina@gmail.com>
2023-09-04 17:46:52 +0300

    little fix

--- src/components/DrinksSearch/DrinksSearch.jsx
+++ src/components/DrinksSearch/DrinksSearch.jsx
@@ -1,38 +1,44 @@
 import { useEffect } from 'react';
-import css from './DrinksSearch.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchCategories, fetchDrinks, fetchIngredients } from 'redux/Drinks/DrinksOperation';
+import { useSearchParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom/dist';
 import Select from 'react-select';
 // import debounce from 'lodash.debounce';
-import { selectStylesCoktails, selectStylesIngredients } from './selectStyles';
-import { useSearchParams } from 'react-router-dom';
+// components
 import Dots from 'components/Spinner/Dots';
 import { Paginator } from 'components/Paginator/Paginator';
-import { useParams } from 'react-router-dom/dist';
 import { SearchSvg } from './additionalComponents';
-import { getAllCategories, getAllIngredients, getDrinks, getIsLoading, getDrinksPage, getTotalPages } from 'redux/Drinks/DrinksSelectors';
 import DrinkItemCard from './DrinkItemCard/DrinkItemCard';
 import { NoRecipe } from 'components/NoRecipe/NoRecipe';
+// redux
+import { fetchCategories, fetchDrinks, fetchIngredients } from 'redux/Drinks/DrinksOperation';
+import { getAllCategories, getAllIngredients, getDrinks, getIsLoading, getDrinksPage, getTotalPages } from 'redux/Drinks/DrinksSelectors';
 import { changeDrinksPage } from 'redux/Drinks/DrinksSlice';
+// styles
+import { selectStylesCoktails, selectStylesIngredients } from './selectStyles';
+import css from './DrinksSearch.module.css';
 
 export const DrinksSearch = () => {
   const dispatch = useDispatch();
+  const { categoryName } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams({ category: categoryName });
+
   const categoryList = useSelector(getAllCategories);
   const ingredientList = useSelector(getAllIngredients);
   const isLoading = useSelector(getIsLoading);
   const entities = useSelector(getDrinks);
-  const { categoryName } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams({ category: categoryName });
+  const currentPage = useSelector(getDrinksPage);
+  const totalPages = useSelector(getTotalPages);
+
   const category = searchParams.get('category') || '';
   const ingredient = searchParams.get('ingredient') || '';
   const q = searchParams.get('q') || '';
   const page = searchParams.get('page') || 1;
-  const currentPage = useSelector(getDrinksPage);
-  const totalPages = useSelector(getTotalPages);
+
+
   const categorySelectOptions = categoryList.map(({ name }) => {
     return { value: `${name}`, label: `${name}` };
   });
-
   const ingredientSelectOptions = ingredientList.map(({ title }) => {
     return { value: `${title}`, label: `${title}` };
   });
@@ -95,7 +101,7 @@ export const DrinksSearch = () => {
         )}
 
         {isLoading && <Dots className={css.loading} />}
-        {entities?.data?.length === 0 && isLoading === false && <NoRecipe title={'No results'} />}
+        {entities.length === 0 && isLoading === false && <NoRecipe title={'No results'} />}
 
         {totalPages > 1 && (
           <Paginator pages={{ page: currentPage, totalPages: totalPages }} onChangePage={changeDrinksPage} />
