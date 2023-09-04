c961563b81e22a436a38e4583b1a64f5f3e94169
AndriiHamasa <andrii.khamaza@nure.ua>
2023-09-04 13:20:58 +0300

    change title on MainTitle

--- src/pages/DrinksPage.js
+++ src/pages/DrinksPage.js
@@ -1,15 +1,12 @@
 import { DrinksSearch } from 'components/DrinksSearch/DrinksSearch';
-// import { MainTitle } from '../components/MainTitle/MainTitle';
+import { MainTitle } from '../components/MainTitle/MainTitle';
 import css from '../components/DrinksSearch/DrinksSearch.module.css';
 
 export default function DrinksPage() {
   return (
     <div className={css.container}>
-      <h1 className={css.title}>Drinks </h1>
-      {/* <MainTitle title="Drinks" className={css.title} /> */}
+      <MainTitle title="Drinks" />
       <DrinksSearch />
     </div>
   );
-
-  // return <DrinksSearch />
 }
