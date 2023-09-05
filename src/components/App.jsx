5257465831c941f1973f86b71fc578f94c091182
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-09-06 01:04:23 +0300

    fix build/a-i: center mobile links footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { lazy, useEffect } from 'react';
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { Spinner } from './Spinner/Spinner';
