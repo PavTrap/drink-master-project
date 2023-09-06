b3ec4d56f160969730108352d52db46047608a06
HalynaSilina <halynasilina@gmail.com>
2023-09-07 02:14:46 +0300

    add border on white buttons on hover

--- src/components/RecipePageHero/RecipePageHero.jsx
+++ src/components/RecipePageHero/RecipePageHero.jsx
@@ -1,26 +1,28 @@
 import RecipePageTitle from './RecipePageTitle/RecipePageTitle';
 import RecipeButton from 'components/RecipePageHero/RecipeButton/RecipeButton';
 
-import css from './RecipePageHero.module.css'
+import css from './RecipePageHero.module.css';
 
 const RecipePageHero = ({ data }) => {
-
-  const { glass, drinkThumb, drink, _id} = data;
-
-
+  const { glass, drinkThumb, drink, _id } = data;
+console.log(data);
   return (
     <section className={css.sectionContainer}>
-        <div className={css.heroContainer}>
-            <p className={css.textGlass}>{glass}</p>
-            <RecipePageTitle title={drink} />
-            {/* звідки брати опис коктейля? написати логіку за відсутності опису */}
-            <p className={css.textDescript}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum excepturi, in illo obcaecati possimus accusantium hic perferendis, provident, fugit explicabo quaerat? Culpa odio magnam maxime facere unde facilis praesentium. Distinctio.
-            Consectetur esse molestias odio accusamus pariatur voluptatibus dicta quidem. Deserunt, enim quis consequuntur minus velit sit reiciendis suscipit quo ad voluptate ipsam culpa neque aspernatur autem porro doloremque quam cum?</p>
-            <RecipeButton id={_id}/>
-        </div>
-        <img className={css.imgDrink} src={drinkThumb} alt={drink} title={drink}/>
+      <div className={css.heroContainer}>
+        <p className={css.textGlass}>{glass}</p>
+        <RecipePageTitle title={drink} />
+        {/* звідки брати опис коктейля? написати логіку за відсутності опису */}
+        <p className={css.textDescript}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum excepturi, in illo obcaecati possimus accusantium hic
+          perferendis, provident, fugit explicabo quaerat? Culpa odio magnam maxime facere unde facilis praesentium. Distinctio. Consectetur
+          esse molestias odio accusamus pariatur voluptatibus dicta quidem. Deserunt, enim quis consequuntur minus velit sit reiciendis
+          suscipit quo ad voluptate ipsam culpa neque aspernatur autem porro doloremque quam cum?
+        </p>
+        <RecipeButton id={_id} />
+      </div>
+      <img className={css.imgDrink} src={drinkThumb} alt={drink} title={drink} />
     </section>
-  )
+  );
 };
 
 export default RecipePageHero;
