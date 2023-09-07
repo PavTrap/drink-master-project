0f1d43ba3f38eb352c4cb542f38cd9ee0bfe6950
HalynaSilina <halynasilina@gmail.com>
2023-09-07 03:26:18 +0300

    add favs page reset to removeFrom Favs btn

--- src/components/RecipePageHero/RecipeButton/RecipeButton.jsx
+++ src/components/RecipePageHero/RecipeButton/RecipeButton.jsx
@@ -3,22 +3,24 @@ import useAuth from 'hooks/useAuth';
 import css from './RecipeButton.module.css';
 import * as API from './btnApi';
 import { toast } from 'react-toastify';
+import { changeFavPage } from 'redux/FavoriteCocktails/FavoritesSlice';
+import { useDispatch } from 'react-redux';
 
 const RecipeButton = ({ id }) => {
   const { userId } = useAuth();
   const [isAddedeToFavorite, setIsAddedeToFavorite] = useState(false);
   const [pending, setPendings] = useState(false);
-
+  const dispatch = useDispatch();
   //check favs field from BackEnd
-  useEffect(()=>{
-    (async ()=>{
-      const isFavsChanged = await API.checkCurrent(id)
+  useEffect(() => {
+    (async () => {
+      const isFavsChanged = await API.checkCurrent(id);
       const foundInFavs = isFavsChanged.some(item => item === userId);
       setIsAddedeToFavorite(foundInFavs);
-    })()
-  },[id, userId])
+    })();
+  }, [id, userId]);
 
-  //Toggle and update 
+  //Toggle and update
   async function handleFavsButtonAction(value) {
     setPendings(true);
     switch (value) {
@@ -27,7 +29,7 @@ const RecipeButton = ({ id }) => {
         if (addRes?.status === 201) {
           setIsAddedeToFavorite(true);
           setPendings(false);
-          addRes?.data?.message && toast.info("Added to Favorites");
+          addRes?.data?.message && toast.info('Added to Favorites');
         }
         break;
       case 'delete':
@@ -35,7 +37,8 @@ const RecipeButton = ({ id }) => {
         if (delRes?.status === 200) {
           setIsAddedeToFavorite(false);
           setPendings(false);
-          delRes?.data?.message && toast.info("Removed from Favorites");
+          delRes?.data?.message && toast.info('Removed from Favorites');
+          dispatch(changeFavPage(1));
         }
         break;
       default:
@@ -59,5 +62,3 @@ const RecipeButton = ({ id }) => {
 };
 
 export default RecipeButton;
-
- 
